d28fa34a109aff53d6a2a71ba565a7474d5b6e72
Andrew Smith <andrewsmith@alumni.stanford.edu>
2020-10-29 21:21:29 -0700

    Add test utils to sandbox.

---
+++ sandbox_react/src/lib/utils.js
@@ -0,0 +1,30 @@
+import { curry } from 'ramda'
+
+/**
+ * Workaround for broken `findWhere` behavior for mounted components when searching by text
+ * https://github.com/airbnb/enzyme/issues/1566
+ */
+const textContent = (node) => {
+  try {
+    // enzyme sometimes blows up on text()
+    return node.text()
+  } catch (_e) {
+    return ''
+  }
+}
+
+// useful for finding a node by its text
+export const findByText = (text, wrapper, options = {}) => {
+  if (!wrapper) throw new Error('Expected a wrapper but did not receive one')
+  const comparator = options.exact
+    ? (x) => textContent(x) === text
+    : (x) => new RegExp(text).test(textContent(x))
+  return wrapper
+    .findWhere(comparator)
+    .filterWhere((x) => x.html().startsWith('<'))
+    .last()
+}
+
+export const sel = curry((dataTestId, wrapper) =>
+  wrapper.find(`[data-test-id="${dataTestId}"]`)
+)
